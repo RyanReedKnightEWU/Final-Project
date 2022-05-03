73ab550e1c9309aeaf83c2d229837465eec00031
hallen10 <hallen10@ewu.edu>
2022-05-03 14:38:43 -0700

    Added a writer to save and a scanner to load.

--- src/SaveLoader/Load.java
+++ src/SaveLoader/Load.java
@@ -1,8 +1,17 @@
 package SaveLoader;
 
+import java.util.Scanner;
+
 public class Load extends SaveLoader{
 
+    Scanner scanner = null;
+
     public Load(){
+        try {
+            scanner = new Scanner(file);
+        } catch (Exception e){
+            System.out.println(e);
+        }
 
     }
 
