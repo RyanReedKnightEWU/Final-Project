2818d28bf167efe87478624e2b66d04f080e6c27
BrendanK <brendan_emblin@hotmail.com>
2022-05-04 10:52:36 -0700

    Added new Goblin constructor

--- src/Entity/Goblin.java
+++ src/Entity/Goblin.java
@@ -8,11 +8,16 @@ import Items.Weapons.BareHands;
 public class Goblin extends Entity {
 
     public Goblin(String name) {
-        //Find a more proper way to let an entity subclass pass its stats to Entity.
         super(75, 10, 3, name);
         Weapon weapon = new BareHands();
         setWeapon(weapon);
         Armor armor = new Clothes(0);
+        setArmor(armor);
+    }
+
+    public Goblin(String name, Weapon weapon, Armor armor) {
+        super(75,10,3,name);
+        setWeapon(weapon);
         setArmor(armor);
     }
 }
