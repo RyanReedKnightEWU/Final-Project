f3148d6949749d0947d789d929440f5a0eae58f6
hallen10 <hallen10@ewu.edu>
2022-05-06 14:26:27 -0700

    Added more to MainMenuScene.

--- src/main/java/GameApplication/MainMenuScene.java
+++ src/main/java/GameApplication/MainMenuScene.java
@@ -3,24 +3,35 @@ package GameApplication;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+
+import java.io.File;
 
 import static FinalProject.Javafx.ApplicationMain.gameWindow;
 import static FinalProject.Javafx.ApplicationMain.scene;
 
 public class MainMenuScene {
-    HBox layout = new HBox();
-    Button newGame, loadGame;
-    //Scene scene;
+    private HBox layout = new HBox();
+    private Button newGame, loadGame;
+
     public static MapScene map;
 
     public void start(){
-        newGame = new Button("Start Game");
+        newGame = new Button("New Game");
         newGame.setOnAction(e -> newGame());
 
         loadGame = new Button("Load Game");
         loadGame.setOnAction(e -> loadGame());
+
+        //FOR TESTING
+        Button skip = new Button("Skip");
+        loadGame.setOnAction(e -> Skip());
+        layout.getChildren().add(skip);
 
         layout.getChildren().add(newGame);
         layout.getChildren().add(loadGame);
@@ -33,10 +44,53 @@ public class MainMenuScene {
     }
 
     private void newGame(){
+        TextField textField;
+        textField = new TextField();
+        Label label = new Label("Enter name of save: ");
 
+        layout = new HBox();
+        layout.getChildren().add(label);
+        layout.getChildren().add(textField);
+        layout.setAlignment(Pos.CENTER);
+        layout.setSpacing(15);
+
+        scene.setOnKeyPressed(e -> {
+            if(e.getCode() == KeyCode.ENTER){
+                newSave(textField.getText());
+            }
+        });
+        scene.setRoot(layout);
+        gameWindow.setScene(scene);
+    }
+
+    private void newSave(String name){
+        System.out.println("Player entered: "+name);
     }
 
     private void loadGame(){
+        String names [];
+        //Gets the location of the java directory
+        String file = new File("").getAbsolutePath();
+        //System.out.println(file);
+
+        names = new File(file+"\\Saves").list();
+        layout = new HBox();
+        layout.setAlignment(Pos.CENTER);
+
+        //List all the names of saves.
+        for (String name: names) {
+            Button button = new Button(name.substring(0,name.indexOf(".")));
+            button.setOnAction(e -> readGameFile(name));
+            layout.getChildren().add(button);
+        }
+
+        layout.setAlignment(Pos.CENTER);
+        layout.setSpacing(15);
+        scene.setRoot(layout);
+        gameWindow.setScene(scene);
+    }
+
+    private void readGameFile(String name){
 
     }
 
@@ -44,5 +98,10 @@ public class MainMenuScene {
         System.out.println("Starting the arena/map.");
         map = new MapScene();
         //arena.Show();
+    }
+
+    //allows us to skip the main menu for testing purposes
+    private void Skip(){
+        startMap();
     }
 }
