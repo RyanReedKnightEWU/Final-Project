0c972477f1bd851969eb158aacefc6cb4aa7ae3d
hallen10 <hallen10@ewu.edu>
2022-05-22 22:46:50 -0700

    Making factories for the shop. Added ways to get different Consumables.

--- src/main/java/gameobjects/Items/Consumables/ConsumableFactory.java
+++ src/main/java/gameobjects/Items/Consumables/ConsumableFactory.java
@@ -1,6 +1,10 @@
 package gameobjects.Items.Consumables;
 
 import gameobjects.Items.Consumable;
+import gameobjects.Items.Weapon;
+import gameobjects.Items.Weapons.*;
+
+import java.util.Random;
 
 public class ConsumableFactory extends ConsumableFactoryBase{
     @Override
@@ -8,9 +12,9 @@ public class ConsumableFactory extends ConsumableFactoryBase{
                                        int heal, int value, String description, int amount) {
 
         if(subclassKey.startsWith(AttackBottle.class.getName())){
-            return new AttackBottle(name, minDamage, maxDamage,heal,value,description,amount);
+            return new Consumable(name, minDamage, maxDamage,heal,value,description,amount);
         }else if(subclassKey.startsWith(healthPotion.class.getName())) {
-            return new healthPotion(name, minDamage, maxDamage,heal,value,description,amount);
+            return new Consumable(name, minDamage, maxDamage,heal,value,description,amount);
         }else if(subclassKey.startsWith(throwingKnife.class.getName())) {
             return new throwingKnife(name, minDamage, maxDamage,heal,value,description,amount);
         }else{
@@ -18,4 +22,47 @@ public class ConsumableFactory extends ConsumableFactoryBase{
         }
 
     }
+
+    public Consumable createRandomConsumable(){
+        Consumable consumable;
+        Random rand = new Random();
+        int type = rand.nextInt(3);
+        int condition = rand.nextInt(3)-1;
+        int amount = rand.nextInt(5)+1;
+        switch (type){
+            default:
+                consumable = new AttackBottle(condition);
+                break;
+            case 1:
+                consumable = new healthPotion(condition);
+                break;
+            case 2:
+                consumable = new throwingKnife();
+                break;
+        }
+        consumable.setAmount(amount);
+        return consumable;
+    }
+
+    public Consumable createRandomConsumable(int limit){
+        Consumable consumable;
+        Random rand = new Random();
+        int type = rand.nextInt(3);
+        int condition = rand.nextInt(3)-1;
+        int amount = rand.nextInt(limit)+1;
+        switch (type){
+            default:
+                consumable = new AttackBottle(condition);
+                break;
+            case 1:
+                consumable = new healthPotion(condition);
+                break;
+            case 2:
+                consumable = new throwingKnife();
+                break;
+        }
+        consumable.setAmount(amount);
+        return consumable;
+    }
+
 }
