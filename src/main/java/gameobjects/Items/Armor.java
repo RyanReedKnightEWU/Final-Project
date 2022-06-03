779c2dc923fe8cc1050a35646ef0384b7ca3fa8d
hallen10 <hallen10@ewu.edu>
2022-06-02 18:37:50 -0700

    Added comments.

--- src/main/java/gameobjects/Items/Armor.java
+++ src/main/java/gameobjects/Items/Armor.java
@@ -3,11 +3,21 @@ package gameobjects.Items;
 import java.util.Random;
 
 public class Armor extends Items{
+    /**
+     * The amount of protection the armor provides.
+     */
     int armorValue = 0;
-
-
+    /**
+     * The amount of armor the armor can vary when the armor is made.
+     */
     int vary = 5;
 
+    /**
+     * Allows for basic construction of armor to be made.
+     * @param name
+     * @param armorValue
+     * @param value
+     */
     protected Armor(String name, int armorValue, int value) {
         type = "Armor";
         setArmorValue(armorValue);
@@ -16,6 +26,13 @@ public class Armor extends Items{
         setDescription("armor");
     }
 
+    /**
+     * Allows one to fully define the armor. For loading purposes.
+     * @param name
+     * @param armorValue
+     * @param value
+     * @param description
+     */
     public Armor(String name, int armorValue, int value, String description) {
         //Used for loading armor
         type = "Armor";
@@ -33,6 +50,10 @@ public class Armor extends Items{
         return armorValue;
     }
 
+    /**
+     * If the armor is the old version then it will have a decreased armor value, value, and a slightly different name.
+     * @param oldName
+     */
     protected void old(String oldName){
         armorValue -= vary;
         setValue(getValue()-getValue()/4);
@@ -41,6 +62,10 @@ public class Armor extends Items{
         setName(temp);
     }
 
+    /**
+     * If the armor is the new version then it will have an increased armor value, value, and a slightly different name.
+     * @param cleanName
+     */
     protected void clean(String cleanName){
         armorValue += vary;
         setValue(getValue()+getValue()/4);
@@ -49,6 +74,7 @@ public class Armor extends Items{
         setName(temp);
     }
 
+    /*
     protected void setCondition(int luck, String cleanName, String oldName){
         Random rand = new Random();
         int good = 20+luck*5;
@@ -61,13 +87,13 @@ public class Armor extends Items{
         }
     }
 
-
     public int getVary() {
         return vary;
     }
     protected void setVary(int vary){
         this.vary = vary;
     }
+     */
 
     public String toString(){
         String value;
@@ -75,6 +101,10 @@ public class Armor extends Items{
         return value;
     }
 
+    /**
+     * Creates a save string for saving.
+     * @return
+     */
     public String save(){
         String value = "ARMOR\n";
         //value += String.format("Name: %s\nArmor: %d\nValue: %d\nDescription: %n", getName(), getArmorValue(), getValue(), getDescription());
